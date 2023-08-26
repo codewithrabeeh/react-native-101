faf3c01e62744478d0f6fea46a8d0837dd0d9b56
codewithrabeeh <rabeehebrahim48@gmail.com>
2023-08-26 22:44:39 +0530

    feat: button

--- App.js
+++ App.js
@@ -1,47 +1,13 @@
-import { View, Text, Image, ScrollView } from "react-native"
+import { View, Button } from "react-native"
 const img = require('./assets/image.jpg')
 
 export default function App() {
 
   return <View style={{ backgroundColor: "beige", flex: 1, padding: 60 }}>
-    {/* 
-        ScrollView is used to scroll the content that overflow.
-        Always wrap content inside View with ScrollView.
-    */}
-    
-    <ScrollView>
-
-      <Image source={img} style={{ width: 300, height: 300 }} />
-
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        voluptatibus, quibusdam, quia, quos voluptatem voluptatum quod
-        consequatur voluptate quas doloribus quidem. Quisquam voluptatibus,
-        quibusdam, quia, quos voluptatem voluptatum quod consequatur voluptate
-        quas doloribus quidem. Quisquam voluptatibus, quibusdam, quia, quos
-        voluptatem voluptatum quod consequatur voluptate quas doloribus quidem.
-        Quisquam voluptatibus, quibusdam, quia, quos voluptatem voluptatum quod
-        consequatur voluptate quas doloribus quidem. Quisquam voluptatibus,
-        quibusdam, quia, quos voluptatem voluptatum quod consequatur voluptate
-        quas doloribus quidem. Quisquam voluptatibus, quibusdam, quia, quos
-        voluptatem voluptatum quod consequatur voluptate quas doloribus quidem.
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        voluptatibus, quibusdam, quia, quos voluptatem voluptatum quod
-        consequatur voluptate quas doloribus quidem. Quisquam voluptatibus,
-        quibusdam, quia, quos voluptatem voluptatum quod consequatur voluptate
-        quas doloribus quidem. Quisquam voluptatibus, quibusdam, quia, quos
-        voluptatem voluptatum quod consequatur voluptate quas doloribus quidem.
-        Quisquam voluptatibus, quibusdam, quia, quos voluptatem voluptatum quod
-        consequatur voluptate quas doloribus quidem. Quisquam voluptatibus,
-        quibusdam, quia, quos voluptatem voluptatum quod consequatur voluptate
-        quas doloribus quidem. Quisquam voluptatibus, quibusdam, quia, quos
-        voluptatem voluptatum quod consequatur voluptate quas doloribus quidem.
-      </Text>
-
-      <Image source={img} style={{ width: 300, height: 300 }} />
-
-    </ScrollView>
-
+    <Button title="Click Me"
+      onPress={() => { console.log('Button Clicked') }}
+      color={'black'} 
+      disabled={true}
+      />
   </View>
 }
