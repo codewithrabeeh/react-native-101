746bf23806da9a7bb34c4e9f446d14dec4d35c16
codewithrabeeh <rabeehebrahim48@gmail.com>
2023-08-26 20:16:02 +0530

    textView

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text} from "react-native"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View style={{backgroundColor: "beige", flex: 1, padding: 60}}>
+    <Text>Hello World</Text>
+  </View> 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
